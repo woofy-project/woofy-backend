ca2e7bd0d1e163b977e33b9f6eb81c9e03dcc505
HS-hyeokjin <youngurwls@gmail.com>
2024-03-19 19:36:28 +0900

    init : Woofy 프로젝트 초기화

---
+++ src/main/java/com/hmpr/woofy/WoofyApplication.java
@@ -0,0 +1,13 @@
+package com.hmpr.woofy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoofyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoofyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hmpr/woofy/WoofyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hmpr.woofy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoofyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
