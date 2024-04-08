58e7166e18e39cf976c33674503972934f1c320a
HS-hyeokjin <youngurwls@gmail.com>
2024-04-08 22:52:00 +0900

    feat : google login 컨트롤러 추가

---
+++ src/main/java/com/hmpr/woofy/auth/service/impl/GoogleAuthServiceImpl.java
@@ -0,0 +1,8 @@
+package com.hmpr.woofy.auth.service.impl;
+
+import com.hmpr.woofy.auth.service.GoogleAuthService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GoogleAuthServiceImpl implements GoogleAuthService {
+}
